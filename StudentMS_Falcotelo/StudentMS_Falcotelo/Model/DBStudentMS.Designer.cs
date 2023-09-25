b170354ac49b25d141cb295e3461bbe019382870
xFalcooo <146010871+xFalcooo@users.noreply.github.com>
2023-09-26 06:38:06 +0800

    Update

--- StudentMS_Falcotelo/StudentMS_Falcotelo/Model/DBStudentMS.Designer.cs
+++ StudentMS_Falcotelo/StudentMS_Falcotelo/Model/DBStudentMS.Designer.cs
@@ -1,4 +1,4 @@
-﻿// T4 code generation is enabled for model 'C:\Users\cyrus\Documents\Github\studentManagement\StudentMS_Falcotelo\StudentMS_Falcotelo\Model\studentManagementDB.edmx'. 
+﻿// T4 code generation is enabled for model 'C:\Users\Benedict\OneDrive\Documents\GitHub\studentManagement\StudentMS_Falcotelo\StudentMS_Falcotelo\Model\DBStudentMS.edmx'. 
 // To enable legacy code generation, change the value of the 'Code Generation Strategy' designer
 // property to 'Legacy ObjectContext'. This property is available in the Properties Window when the model
 // is open in the designer.
